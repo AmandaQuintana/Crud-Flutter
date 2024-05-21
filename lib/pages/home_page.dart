5fbce8283bac3c48b51bf28bfccf72f176b068d4
Amanda Quintana <aquintana@lsv-tech.com>
2024-05-21 09:24:52 -0500

    Metodo delete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,20 +12,20 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-      title: const Text(
-        'Home Page',
-        style: TextStyle(
-          color: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'Home Page',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
-      ),
-      backgroundColor: Colors.deepPurpleAccent,
+        backgroundColor: Colors.deepPurpleAccent,
       ),
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: getUsers(),
         builder: (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
+            return const Center(
               child: CircularProgressIndicator(),
             );
           }
@@ -41,26 +41,68 @@ class _HomeState extends State<Home> {
               final userData = users[index];
               return Card(
                 elevation: 3,
-                margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                child: ListTile(
-                  title: Text(
-                    userData['nombre'] ?? '',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Cedula: ${userData['cedula'] ?? ''}'),
-                      Text('Correo: ${userData['correo'] ?? ''}'),
-                    ],
-                  ),
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/update',
-                      arguments: userData,
-                    );
+                margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child: Dismissible(
+                  onDismissed: (direction) async {
+                    await deleteUser(userData['id']);
+                    setState(() {
+                      users.removeAt(index);
+                    });
                   },
+                  confirmDismiss: (direction) async {
+                    bool result = false;
+
+                    result = await showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('¿Está seguro de querer eliminar el usuario?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context, false);
+                              },
+                              child: const Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context, true);
+                              },
+                              child: const Text('Eliminar'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+
+                    return result;
+                  },
+                  background: Container(
+                    color: const Color.fromARGB(255, 217, 216, 216),
+                    child: const Icon(Icons.delete),
+                  ),
+                  direction: DismissDirection.startToEnd,
+                  key: Key(userData['id']),
+                  child: ListTile(
+                    title: Text(
+                      userData['nombre'] ?? '',
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Cedula: ${userData['cedula'] ?? ''}'),
+                        Text('Correo: ${userData['correo'] ?? ''}'),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/update',
+                        arguments: userData,
+                      );
+                    },
+                  ),
                 ),
               );
             },
